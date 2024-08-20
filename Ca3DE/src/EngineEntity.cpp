da3717fcab2e3c50d9a6c6a374313672bce94c2f
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2024-08-20 10:34:34 +0200

    yep :D

--- Ca3DE/src/EngineEntity.cpp
+++ Ca3DE/src/EngineEntity.cpp
@@ -1,9 +1,3 @@
-/*
-Cafu Engine, http://www.cafu.de/
-Copyright (c) Carsten Fuchs and other contributors.
-This project is licensed under the terms of the MIT license.
-*/
-
 #include "EngineEntity.hpp"
 #include "NetConst.hpp"
 #include "ConsoleCommands/ConVar.hpp"
--- Ca3DE/src/NetConst.hpp
+++ Ca3DE/src/NetConst.hpp
@@ -1,37 +1,31 @@
-/*
-Cafu Engine, http://www.cafu.de/
-Copyright (c) Carsten Fuchs and other contributors.
-This project is licensed under the terms of the MIT license.
-*/
-
 /*****************************/
 /*** Net Message Constants ***/
 /*****************************/
 
 
-// Dieses Header-File definiert die Konstanten für die Kommunikation zwischen Server und Client.
-// Die 0 wurde absichtlich freigehalten, um sie später evtl. als ErrorFlag verwenden zu können.
+// This header file defines the constants for communication between the server and client.
+// The 0 was deliberately left blank so that it can be used later as an error flag if necessary.
 
-// 'connection-less' Message-Types vom Client zum Server
+// 'connection-less' message types from the client to the server
 const char CS0_NoOperation         =1;
 const char CS0_Ping                =2;
 const char CS0_Connect             =3;
 const char CS0_Info                =4;
 const char CS0_RemoteConsoleCommand=5;  ///< A message consisting of a password and a command string that is to be executed by the server console interpreter.
 
-// 'connection-less' Message-Types vom Server zum Client
+// 'connection-less' message types from the server to the client
 const char SC0_ACK           =1;
 const char SC0_NACK          =2;
 const char SC0_RccReply      =3;        ///< String reply to a CS0_RemoteConsoleCommand message.
 
-// 'connection-established' Message-Types vom Client zum Server
+// 'connection-established' message types from client to server
 const char CS1_PlayerCommand =1;
 const char CS1_Disconnect    =2;
 const char CS1_SayToAll      =3;
 const char CS1_WorldInfoACK  =4;
 const char CS1_FrameInfoACK  =5;
 
-// 'connection-established' Message-Types vom Server zum Client
+// 'connection-established' message types from the server to the client
 const char SC1_WorldInfo     =1;
 const char SC1_EntityBaseLine=2;
 const char SC1_FrameInfo     =3;
