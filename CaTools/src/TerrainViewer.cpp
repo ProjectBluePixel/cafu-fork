7161f57735cbb81363f60737a70ad11335ddee0d
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2024-08-29 21:22:49 +0200

    wip

--- CaTools/src/TerrainViewer.cpp
+++ CaTools/src/TerrainViewer.cpp
@@ -1,8 +1,3 @@
-/*
-Cafu Engine, http://www.cafu.de/
-Copyright (c) Carsten Fuchs and other contributors.
-This project is licensed under the terms of the MIT license.
-*/
 #include <stdio.h>
 #include <float.h>
 #include <string.h>
--- CaWE/src/DialogOptions.cpp
+++ CaWE/src/DialogOptions.cpp
@@ -1,9 +1,3 @@
-/*
-Cafu Engine, http://www.cafu.de/
-Copyright (c) Carsten Fuchs and other contributors.
-This project is licensed under the terms of the MIT license.
-*/
-
 #include "DialogOptions.hpp"
 #include "GameConfig.hpp"
 #include "Options.hpp"
